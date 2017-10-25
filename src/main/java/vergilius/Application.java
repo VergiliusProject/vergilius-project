483a34861b1148787c641780af4dc58ca954f2c3
RevJay <lana.preppy_modern@mail.ru>
2017-10-25 12:07:10 +0300

    Initial commit

--- src/main/java/vergilius/Application.java
+++ src/main/java/vergilius/Application.java
@@ -1,6 +1,5 @@
 package vergilius;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/vergilius/Greeting.java
@@ -0,0 +1,24 @@
+package vergilius;
+
+
+public class Greeting {
+    private String nickname;
+    private String message;
+
+    public String getNickname()
+    {
+        return nickname;
+    }
+    public void setNickname(String nickname)
+    {
+        this.nickname = nickname;
+    }
+    public String getMessage()
+    {
+        return message;
+    }
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
