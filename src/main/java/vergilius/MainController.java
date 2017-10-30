ed2a087a355e40452c060e5dd837bdd94ebb0651
RevJay <lana.preppy_modern@mail.ru>
2017-10-30 20:07:56 +0200

    1. Thymeleaf template engine was replaced with Jade4j. Necessary dependencies were added to build.gradle file. 2. MainController is used now for handling form in the same way as GreetingController. 3. Instead of greeting.html and result.html, MainController work with jade files(jgreeting, jresult) 4. Also in IDEA I added File Watcher for jade files.

--- src/main/java/vergilius/MainController.java
+++ src/main/java/vergilius/MainController.java
@@ -1,24 +1,22 @@
 package vergilius;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import vergilius.Greeting;
 
 @Controller
-public class GreetingController {
-/*
-    @GetMapping("/greeting")
+public class MainController {
+
+    @GetMapping("/")
     public String greetingForm(Model model) {
         model.addAttribute("greeting", new Greeting());
-        return "greeting";
+        return "jgreeting";
     }
-
-    @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
-        return "result";
+    @PostMapping("/")
+    public String greetingSubmit(@ModelAttribute Greeting greeting)
+    {
+        return "jresult";
     }
-*/
 }
