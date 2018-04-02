74ceca33d9a508a7bc3b06137abdd84631a93dd3
RevJay <lana.preppy_modern@mail.ru>
2018-04-02 17:01:48 +0300

    Some Bootstrap's features and menu for each webpage were added.

--- src/main/java/vergilius/UnionConverter.java
+++ src/main/java/vergilius/UnionConverter.java
@@ -5,6 +5,6 @@ import vergilius.repos.TtypeRepository;
 public class UnionConverter {
     public static String converts(Ttype myUnion, TtypeRepository rep2)
     {
-        return FieldBuilder.recoursionProcessing(rep2, myUnion, 0).toString() + ";";
+        return FieldBuilder.recoursionProcessing(rep2, myUnion, 0).toString().trim()  + ";";
     }
 }
