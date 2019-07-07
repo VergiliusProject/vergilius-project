188e69966bf00b068c6f42c2808b1ef1cd08ebb6
SergiusTheBest <sergey.podobry@gmail.com>
2019-07-07 16:03:32 +0300

    Change error reporting in generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 ./gradlew bootRun &
 sleep 15s
@@ -8,15 +8,16 @@ git clone https://github.com/VergiliusProject/kernels-data.git
 for f in kernels-data/yml/*.yml
 do
     echo importing $f
-    #curl -s -F "file=@$f" http://localhost:8080/admin
+    curl -s -F "file=@$f" http://localhost:8080/admin
 done
 
+set +e
+
 (while true; do echo '[sending keep alive]'; sleep 60s; done;) &
-#wget -q --mirror --page-requisites -E http://localhost:8080/
+wget -q --mirror --page-requisites -E http://localhost:8080/
 wget -q --mirror http://localhost:8080/BingSiteAuth.xml
 wget -q --mirror http://localhost:8080/.nojekyll
 wget -q --mirror http://localhost:8080/CNAME
-
-set +e
 wget -q --mirror --page-requisites --content-on-error -E http://localhost:8080/404
+
 jobs -p | xargs kill -9
