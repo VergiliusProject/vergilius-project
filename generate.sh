3009598c39e3f9814c9323a9f1ee77be981e7d4a
SergiusTheBest <sergey.podobry@gmail.com>
2019-07-07 15:37:54 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -8,13 +8,17 @@ git clone https://github.com/VergiliusProject/kernels-data.git
 for f in kernels-data/yml/*.yml
 do
     echo importing $f
-    curl -F "file=@$f" http://localhost:8080/admin
+    curl -s -F "file=@$f" http://localhost:8080/admin
 done
 
 (while true; do echo '[sending keep alive]'; sleep 60s; done;) &
 wget -q --mirror --page-requisites -E http://localhost:8080/
-wget -q --mirror --page-requisites --content-on-error -E http://localhost:8080/404 || true
 wget -q --mirror http://localhost:8080/BingSiteAuth.xml
 wget -q --mirror http://localhost:8080/.nojekyll
 wget -q --mirror http://localhost:8080/CNAME
+
+set +e
+wget -q --mirror --page-requisites --content-on-error -E http://localhost:8080/404
+set -e
+
 jobs -p | xargs kill -9
